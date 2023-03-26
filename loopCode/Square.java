5b355b04d3e6e2a80695822814f45069f910edf2
Maharshi <maharshi1304@gmail.com>
2023-03-26 20:03:06 +0530

    new code

---
+++ loopCode/Square.java
@@ -0,0 +1,12 @@
+
+//Print Square 4*4 matrix pattern:
+
+public class Square {
+    public static void main(String[] args) {
+        for( int row = 1; row <= 4 ; row ++){
+            System.out.println(" **** ");
+        }
+        
+    }
+    
+}
