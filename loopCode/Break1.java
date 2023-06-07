d1aa3771020b83213d0ad157d9cdb7926fdce4aa
Maharshi <maharshi1304@gmail.com>
2023-06-07 16:12:09 +0530

    new code

---
+++ loopCode/Break1.java
@@ -0,0 +1,14 @@
+
+//Pri t 1-5 number if 3 comes then out from lopp using " Break Stmts"
+public class Break1 {
+    public static void main(String[] args) {
+        for( int i =1; i<=5; i++){
+            if( i==3){
+               // continue or break;
+            }
+            System.out.println(i);
+        }
+          System.out.println(" i am out from loop..");
+    }
+    
+}
