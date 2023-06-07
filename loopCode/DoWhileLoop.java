b5a89fe464e246b978b2670644ad2672106c09fb
Maharshi <maharshi1304@gmail.com>
2023-06-07 16:10:41 +0530

    new code

---
+++ loopCode/DoWhileLoop.java
@@ -0,0 +1,13 @@
+//Print Helo-World 10 times
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.print( "Hello-world");
+        
+             i++;
+        }
+             while ( i<=100);
+          
+    }    
+}
